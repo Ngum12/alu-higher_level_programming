af914fd5e5e85e97d0a4c7b3a0965a5dfd1c9135
Ngum12 <d.ngum@alustudent.com>
2023-07-11 02:17:25 -0700

    Python - if/else, loops, functions

--- python-if_else_loops_functions/2-print_alphabet.py
+++ python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-# 2-print_alphabet.py
-
-"""Print the alphabet in lowercase, not followed by a new line."""
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end=""
+for ascii_code in range(ord('a'), ord('z')+1):
+    print("{}".format(chr(ascii_code)), end='')
