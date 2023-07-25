a0fbde95fa83dffc4045fcca6daeedcce31f63cd
Ngum12 <d.ngum@alustudent.com>
2023-07-25 09:40:34 -0700

    Python - Input/Output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python3
-"""Reading a text file"""
+"""function that reads a text file (UTF8) and prints it to stdout"""
 
 
 def read_file(filename=""):
-    """A functions that reads a text file and prints it to stdout
-
-    Args:
-        filename (str): The name of the file to read from.
-
-    """
-
+    """Print the contents of a UTF8 text file to stdout."""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
