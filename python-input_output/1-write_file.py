3e160c9af9fbf3cd90116d9c4f43068d0d655683
Ngum12 <d.ngum@alustudent.com>
2023-07-25 09:33:01 -0700

    Python - Input/Output

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
-"""
-Module 1-number_of_lines
-
-Contains function that returns number of lines in text"""
+"""Writing in a text file"""
 
 
 def write_file(filename="", text=""):
-    """
-    Function that writes to a text file.
+    """A functions that writes a string to a text file and returns the number
+     of characters written
+
     Args:
-        filename: filename
-        text: text to write
-    Raises:
-        Exception: when the file can be opened
+        filename (str): The name of the file to write to.
+        text (str): The string to write to the file.
+    Returns:
+        The number of characters written.
     """
 
-    with open(filename, "w", encoding="utf-8") as f:
+    with open(filename, 'w', encoding="utf-8") as f:
         return f.write(text)
