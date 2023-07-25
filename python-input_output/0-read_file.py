00aa05e85ff7f4cfa212f5f5c9cd9cc1f0e00047
Ngum12 <d.ngum@alustudent.com>
2023-07-25 09:38:13 -0700

    Python - Input/Output

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""
-Module 1-number_of_lines
-
-Contains function that returns number of lines in text"""
+"""Reading a text file"""
 
 
-def write_file(filename="", text=""):
-    """
-    Function that writes to a text file.
+def read_file(filename=""):
+    """A functions that reads a text file and prints it to stdout
+
     Args:
-        filename: filename
-        text: text to write
-    Raises:
-        Exception: when the file can be opened
+        filename (str): The name of the file to read from.
+
     """
 
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
