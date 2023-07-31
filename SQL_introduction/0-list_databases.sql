53f0455cbecfadca4e237d4ba7f9b3dd735b6c49
Ngum12 <d.ngum@alustudent.com>
2023-07-31 09:13:51 -0700

    SQL - Introduction

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,11 +1,6 @@
-#!/usr/bin/env bash
-
-# Connect to the MySQL server
-mysql -hlocalhost -uroot -p
-
-# List all databases
-echo "Database"
-echo "---------"
-for db in $(SHOW DATABASES); do
-    echo "$db"
+!/usr/bin/env bash
+mysql -h localhost -u root -p -e "SHOW DATABASES;" | while read -r db; do
+    if [[ "$db" != "Database" ]]; then
+        echo "$db"
+    fi
 done
