ef33cc46836b1cc84d154f442d4858cf2bf32467
Ngum12 <d.ngum@alustudent.com>
2023-07-31 09:05:23 -0700

    SQL - Introduction

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,16 +1,11 @@
--- 1. Insert a new record with all three fields.
-INSERT INTO users (id, name, age)
-VALUES (2, "Betty", 30);
+#!/usr/bin/env bash
 
--- 2. Insert a new record with the id and name fields, and default the age field to NULL.
-INSERT INTO users (id, name)
-VALUES (2, "Betty");
+# Connect to the MySQL server
+mysql -hlocalhost -uroot -p
 
--- 3. Insert a new record with the id, name, and age fields, but omit the parentheses around the values.
-INSERT users (id, name, age)
-2, "Betty", 30;
-
--- 4. Insert a new record with the id, name, and age fields, but use single quotes around the values.
-INSERT INTO users (id, name, age)
-VALUES (2, 'Betty', 30);
-
+# List all databases
+echo "Database"
+echo "---------"
+for db in $(SHOW DATABASES); do
+    echo "$db"
+done
