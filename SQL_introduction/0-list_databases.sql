0564e29d74467d772c01c10c1835183b52d9193f
Ngum12 <d.ngum@alustudent.com>
2023-07-31 09:20:19 -0700

    SQL - Introduction

--- SQL_introduction/0-list_databases.sql
+++ SQL_introduction/0-list_databases.sql
@@ -1,8 +1,2 @@
 -- command that lists all databases of your MySQL server.
 SHOW DATABASES
-!/usr/bin/env bash
-mysql -h localhost -u root -p -e "SHOW DATABASES;" | while read -r db; do
-    if [[ "$db" != "Database" ]]; then
-        echo "$db"
-    fi
-done
